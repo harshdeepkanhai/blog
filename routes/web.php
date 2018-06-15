2f22afcc0a35d8ba484649af7d169c00f2fdc49c
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2018-06-15 07:45:40 +0530

    build query

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,16 @@
 |
 */
 
-Route::get('/', function () {
-    $tasks = [
-      'Go to the Store',
-      'Finish my screencast',
-      'Clean the House'
-    ];
-    return view('welcome', compact('tasks'));
+Route::get('/tasks', function () {
+    $tasks = DB::table('tasks')->latest()->get();
+
+    return view('tasks.index', compact('tasks'));
+
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/tasks/{task}', function ($id) {
+    $task = DB::table('tasks')->find($id);
+
+    return view('tasks.show', compact('task'));
+
 });
